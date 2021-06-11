29645d8ed21b0142f997608672c4f38b5e886512
Aakash Patel <45399068+aakashPatel123345@users.noreply.github.com>
2021-06-11 16:16:49 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+def login(username, password):
+    pass
+
+def register(username, password):
+    name = input("Enter your username")
+    password = input("Enter your password")
+    verifyPassword = input("Re-enter your password")
+
+def access():
+    pass
