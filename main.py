05226852baddf82f6b48d72dc8f14c465aef2b9f
Aakash Patel <45399068+aakashPatel123345@users.noreply.github.com>
2021-06-18 10:57:20 -0400

    Commit 2

--- main.py
+++ main.py
@@ -1,13 +1,60 @@
+grant = False
+def grant():
+    global granted
+    granted = True
+
+
+
 def login(username, password):
-    pass
+    success = False
+    file = open("user_details.txt","a")
+    for i in file:
+        a,b = i.split(",")
+        b = b.strip()
+        if(username == a and password == b):
+            success = True
+            break
+    file.close()
+    if(success):
+        print("Login Successful")
+        grant()
+    else:
+        print("Login Failed: Wrong Username or Password")
+
+
+            
 
 def register(username, password):
-    name = input("Enter your username")
-    password = input("Enter your password")
-    verifyPassword = input("Re-enter your password")
+    file = open("user_details.txt","a")
+    file.write("\n"+username+","+password)
+    file.close()
+    grant()
 
-def access():
-    pass
+def begin():
+    global option
+    print("Aakash's First Python Project: Welcome!")
+    option = input("Login or Register (L or R) ")
+    if(option!="L" and option!="R"):
+        begin()
 
-def reEnterPassword():
-    pass
+def access(option):
+    global username
+    if(option == "L"):
+        username = input("Enter your username:")
+        password = input("Enter your password: ")
+        login(username,password)
+    else:
+        print("Enter your username and password to register")
+        username = input("Enter your username")
+        password = input("Enter your password")
+        register(username,password)
+
+
+    
+
+begin()
+access(option)
+if(grant):
+    print("Welcome to this website")
+    print("### User Details ####")
+    print("Username: ",username)
